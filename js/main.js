eebfe812d664f5ed03c1174cee73c8fba9ef61a6
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-22 14:24:21 -0500

    More sidebar animations

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
     $('.project-item').hide();
 
     $('.content-card').hide();
+    $('.glyphicon-sunglasses').hide();
 })
 
 // Hide/show project links in sidebar
@@ -20,17 +21,36 @@ $('.projects-link').click(() => {
     }
 })
 
+// Animate sidebar links
+$('.sidebar-nav a').mouseover((e) => {
+    $(e.target).animate({
+        left: '+=5'
+    }, 150);
+})
+
+$('.sidebar-nav a').mouseout((e) => {
+    $(e.target).animate({
+        left: '-=5'
+    }, 150)
+})
+
 // Hide/show content-cards when clicked in sidebar
 $current = null
+$currentLink = null
 
-$('.sidebar nav a').click((e) => {
+$('.sidebar-nav a').click((e) => {
     $clicked = $(e.target);
     if ($clicked.attr('show') !== 'none') {
-        next = $clicked.attr('show')
-        if ($current !== null) {
-            $current.slideUp(250)
+        if ($currentLink !== null) {
+            $currentLink.children('.glyphicon-sunglasses').fadeOut(150)
         }
-        $(next).slideDown(250)
-        $current = $(next)
+        $clicked.children('.glyphicon-sunglasses').fadeIn(150)
+        $currentLink = $clicked
+        next = $clicked.attr('show');
+        if ($current !== null) {
+            $current.slideUp(250);
+        }
+        $(next).slideDown(250);
+        $current = $(next);
     }
 })
