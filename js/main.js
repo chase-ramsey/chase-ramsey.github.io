7e27cc12fe9da44e8803b8921fe1e10a867d0a01
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-22 13:09:02 -0500

    Sliding works on content cards

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(() => {
     $('.projects-list').hide();
     $('.project-item').hide();
+
+    $('.content-card').hide();
 })
 
+// Hide/show project links in sidebar
 var projectsShow = false;
 
 $('.projects-link').click(() => {
@@ -14,5 +17,20 @@ $('.projects-link').click(() => {
         $('.projects-list').slideUp();
         $('.project-item').fadeOut();
         projectsShow = false;
+    }
+})
+
+// Hide/show content-cards when clicked in sidebar
+$current = null
+
+$('.sidebar nav a').click((e) => {
+    $clicked = $(e.target);
+    if ($clicked.attr('show') !== 'none') {
+        next = $clicked.attr('show')
+        if ($current !== null) {
+            $current.slideUp(250)
+        }
+        $(next).slideDown(250)
+        $current = $(next)
     }
 })
